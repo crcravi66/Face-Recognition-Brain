b62ab1e3231309ecca5978bcf63205ac3ba5314d
arun <crcravi66@gmail.com>
2024-04-25 17:34:42 +0530

    started for face recoganition brain project

---
+++ face-recognition-brain/src/App.jsx
@@ -0,0 +1,14 @@
+import { Component, useState } from 'react'
+import './App.css'
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <navigation/>
+      </div>
+    );
+  }
+}
+export default App
